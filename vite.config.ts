2b1e1f8de629952ff45159d905ba5ce14b8e0ea1
xawnu <xawnu@hotmail.com>
2026-04-07 18:52:39 +0800

    fix: allow vite preview to serve external hosts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts: true,
+      },
+      preview: {
+        port: 3000,
+        host: '0.0.0.0',
+        allowedHosts: true,
       },
       plugins: [react()],
       define: {
