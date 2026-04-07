82ddfeeb2655f597a1ecd49e98168e42a66be309
xawnu <xawnu@hotmail.com>
2026-04-07 20:15:43 +0800

    fix: express 5 wildcard route crash

--- server.js
+++ server.js
@@ -70,8 +70,12 @@ const distPath = path.join(__dirname, 'dist');
 app.use(express.static(distPath));
 
 // Fallback for SPA routing - serve index.html for unknown routes
-app.get('*', (req, res) => {
-    res.sendFile(path.join(distPath, 'index.html'));
+app.use((req, res, next) => {
+    if (req.method === 'GET' && !req.path.startsWith('/api/')) {
+        res.sendFile(path.join(distPath, 'index.html'));
+    } else {
+        next();
+    }
 });
 
 // Start Server
